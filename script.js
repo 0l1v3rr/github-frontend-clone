5954f75aa64be30ab99d07a640a53b7c681ebe49
Olivér <oliver.mrakovics@gmail.com>
2022-02-19 08:56:25 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('click', e => {
+    const isDropdownBtn = e.target.matches("[data-dropdown-btn]");
+    if(!isDropdownBtn && e.target.closest('[data-dropdown]') != null) return;
+
+    if(isDropdownBtn) {
+        e.target.closest('[data-dropdown]').classList.toggle('active');
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if(dropdown === e.target.closest('[data-dropdown]')) return;
+        dropdown.classList.remove('active');
+    });
+}, false);
